089eec5449faef76f5b334d5b6b989c2f24d14d3
Fz138726 <61558262+Fz138726@users.noreply.github.com>
2020-02-29 00:19:57 +0800

    Add files via upload

---
+++ Calculator2/Calculator2/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Calculator2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.labelNum1 = new System.Windows.Forms.Label();
+            this.labelNum2 = new System.Windows.Forms.Label();
+            this.labelOperator = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "计算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(372, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(372, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(372, 207);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 3;
+            // 
+            // labelNum1
+            // 
+            this.labelNum1.AutoSize = true;
+            this.labelNum1.Location = new System.Drawing.Point(299, 101);
+            this.labelNum1.Name = "labelNum1";
+            this.labelNum1.Size = new System.Drawing.Size(35, 12);
+            this.labelNum1.TabIndex = 4;
+            this.labelNum1.Text = "数字1";
+            // 
+            // labelNum2
+            // 
+            this.labelNum2.AutoSize = true;
+            this.labelNum2.Location = new System.Drawing.Point(299, 146);
+            this.labelNum2.Name = "labelNum2";
+            this.labelNum2.Size = new System.Drawing.Size(35, 12);
+            this.labelNum2.TabIndex = 5;
+            this.labelNum2.Text = "数字2";
+            // 
+            // labelOperator
+            // 
+            this.labelOperator.AutoSize = true;
+            this.labelOperator.Location = new System.Drawing.Point(299, 216);
+            this.labelOperator.Name = "labelOperator";
+            this.labelOperator.Size = new System.Drawing.Size(41, 12);
+            this.labelOperator.TabIndex = 6;
+            this.labelOperator.Text = "运算符";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(324, 318);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(41, 12);
+            this.labelResult.TabIndex = 7;
+            this.labelResult.Text = "结果：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelOperator);
+            this.Controls.Add(this.labelNum2);
+            this.Controls.Add(this.labelNum1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label labelNum1;
+        private System.Windows.Forms.Label labelNum2;
+        private System.Windows.Forms.Label labelOperator;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ Calculator2/Calculator2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1;
+            double num2;
+            try
+            {
+                num1 = double.Parse(textBox1.Text);
+                num2 = double.Parse(textBox2.Text);
+            }
+            catch
+            {
+                labelResult.Text = "请输入数字";
+                return;
+            }
+
+            switch (textBox3.Text)
+            {
+                case "+":
+                    labelResult.Text = "结果：" + (num1 + num2);
+                    break;
+                case "-":
+                    labelResult.Text = "结果：" + (num1 - num2);
+                    break;
+                case "*":
+                    labelResult.Text = "结果：" + (num1 * num2);
+                    break;
+                case "/":
+                    if (num2 != 0)
+                        labelResult.Text = "结果：" + (num1 / num2);
+                    else
+                        labelResult.Text = "0不能作为除数";
+                    break;
+                default:labelResult.Text="请从“+”“-”“*”“/”中选择";
+                    break;
+            }
+        }
+    }
+}
