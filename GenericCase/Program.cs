7f189e317ad5a01f1cd8a68bede9b9cae87b95eb
Fz138726 <61558262+Fz138726@users.noreply.github.com>
2020-03-15 14:05:25 +0800

    Add files via upload

---
+++ GenericCase/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> p = head; p != null; p = p.Next)
+                action(p.Data);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+
+            // 字符串型List
+            GenericList<string> strList = new GenericList<string>();
+            for (int x = 0; x < 10; x++)
+            {
+                strList.Add("str" + x);
+            }
+
+            strList.ForEach(m => Console.WriteLine(m));
+
+            int max = intlist.Head.Data, min = intlist.Head.Data, sum = 0;
+
+            intlist.ForEach(m => {
+                Console.WriteLine(m);
+                if (max < m) max = m;
+                if (min > m) min = m;
+                sum += m;
+                });
+            Console.WriteLine($"intList 最大值为{max}，最小值为{min}，和为{sum}");
+
+        }
+
+    }
+}
